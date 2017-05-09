4d516a3fd63a371effa7a3529c85c89f4878f1b9
Aditya Atul Tirodkar <tirodkar@google.com>
2017-05-09 16:58:46 -0700

    Add a sample example for running tests in the app context

---
+++ Tests/FunctionalTests/DummyTestTarget/DummyTestTarget.m
@@ -0,0 +1,39 @@
+//
+//  DummyTestTarget.m
+//  DummyTestTarget
+//
+//  Created by Aditya Atul Tirodkar on 5/9/17.
+//  Copyright © 2017 Google Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DummyTestTarget : XCTestCase
+
+@end
+
+@implementation DummyTestTarget
+//
+//- (void)setUp {
+//    [super setUp];
+//    // Put setup code here. This method is called before the invocation of each test method in the class.
+//}
+//
+//- (void)tearDown {
+//    // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    [super tearDown];
+//}
+//
+//- (void)testExample {
+//    // This is an example of a functional test case.
+//    // Use XCTAssert and related functions to verify your tests produce the correct results.
+//}
+//
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+
+@end
